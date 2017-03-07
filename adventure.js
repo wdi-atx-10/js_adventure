0c455bfc786364fc2ce0286f6b4bfb98f0777b32
b b <bboutette@gmail.com>
2017-03-06 23:50:11 -0600

    Here is the homework where I realized too late that I didn't read the directions.

---
+++ adventure.js
@@ -0,0 +1,140 @@
+var deathMethod = prompt ('Do you want to die by hippos, snakes, or lions?')
+
+if (deathMethod == "hippos"){
+  var hippoType = prompt ('Ok, death by hippos it is.  Do you want death by cuddly or courageous hippos?')
+  if (hippoType = "cuddly"){
+    console.log(" cuddlin' time!,-.____,-.
+                                /   ..   \
+                               /_        _\
+                              |'o'      'o'|
+                             / ____________ \
+                           , ,'    `--'    '. .
+                          _| |              | |_
+                        /  ' '              ' '  \
+                       (    `,',__________.','    )
+                        \_    ` ._______, '     _/
+                           |                  |
+                           |    ,-.    ,-.    |
+                            \      ).,(      /
+                       gpyy   \___/    \___/
+")
+    } else {
+      console.log("O.o SO BRAVE.
+
+      .-''''-. _
+ ('    '  '0)-/)
+ '..____..:    \._
+   \u  u (        '-..------._
+   |     /      :   '.        '--.
+  .nn_nn/ (      :   '            '\
+ ( '' '' /      ;     .             \
+  ''----' "\          :            : '.
+         .'/                           '.
+        / /                             '.
+       /_|       )                     .\|
+         |      /\                     . '
+         '--.__|  '--._  ,            /
+                      /'-,          .'
+                     /   |        _.'
+                snd (____\       /
+                          \      \
+                           '-'-'-'    ")
+    }
+
+   }
+
+if (deathMethod == "snakes" ){
+  var snakeType = prompt ('Ok, death by snakes it is. Do you want silly or slithery snakes?')
+ } if (snakeType = "silly"){
+    console.log("           /^\/^\
+                          _|__|  O|
+                 \/     /~     \_/ \
+                  \____|__________/  \
+                         \_______      \
+                                 `\     \                 \
+                                   |     |                  \
+                                  /      /                    \
+                                 /     /                       \\
+                               /      /                         \ \
+                              /     /                            \  \
+                            /     /             _----_            \   \
+                           /     /           _-~      ~-_         |   |
+                          (      (        _-~    _--_    ~-_     _/   |
+                           \      ~-____-~    _-~    ~-_    ~-_-~    /
+                             ~-_           _-~          ~-_       _-~
+                                ~--______-~                ~-___-~")
+  } else {
+    console.log("
+   _______
+  / _   _ \
+ / (.) (.) \
+( _________ )
+ \`-V-|-V-'/
+  \   |   /
+   \  ^  /
+    \    \
+     \    `-_
+      `-_    -_
+         -_    -_
+Max         _-    _-
+       _-    _-
+     _-    _-
+   _-    _-
+   -_    -_
+     -_    -_
+       -_    -_
+         -_    -_
+         _-    _-
+,-=:_-_-_-_ _ _-_-_-_:=-.
+/=I=I=I=I=I=I=I=I=I=I=I=I=\
+|=I=I=I=I=I=I=I=I=I=I=I=I=I=|
+|I=I=I=I=I=I=I=I=I=I=I=I=I=I|
+\=I=I=I=I=I=I=I=I=I=I=I=I=I=/
+\=I=I=I=I=I=I=I=I=I=I=I=I=/
+\=I=I=I=I=I=I=I=I=I=I=I=/
+\=I=I=I=I=I=I=I=I=I=I=/
+ \=I=I=I=I=I=I=I=I=I=/
+  `================='")
+  }
+
+   if (deathMethod == "lions" ){
+      var lionType = prompt ("Ok, death by lions it is.  Do you want cowardly or kingly lions?")
+    } if (lionType = "cowardly" || "kingly") {
+      console.log("Sorry, no choice for you.  All lions are KINGLY, SUCKAHHH
+        .u:o. -c:o.  ex::u.    .czeez* .edB$ e@$$eu
+  e$MMMMMNu^$MMMb.#BMMM$c $MMM8P.d$RM$F4RMMMMMMRb
+A$MMMMMMMMRb^$MMMMb^$MMMP MMMMF4$MMM8"dRMMMMMMMMMN
+zMM8M***M$8M$.#8MMM$.$8M&J$M$%$RMM8*.$R8$#"""""BMM
+$$".e@Rmu. "*M"    '    ^^             .o$$RMM$c'$
+$.$RMMMMMM$$$$ dRRRRRRRRRR$$MMMMMMRL'$$RMMMMMMMM$.
+.$MMMMMMMMM$" $RMMMMMMMMMMMMMMMMMMMMb ^4$MMMMMMMM$
+JMMMM$$**" ..$MMMMMMMMMMMMMMMMMMMMMMM$.:c  "***$MM
+$M"..oenR$".$MMMMMMMMMMMMMMMMMMMMMMMMM$.*$$MMMRc.*
+* d$MMMM$"u$MMMMMMMMMMMMMMMMMMMMMMMMM8MRc"$MMMMM$b
+.$RMMM$# J$MF       "MMMMMMMMM   .....4M$b "$MMMM$
+dMM8P"  dMMM$ $M8P4 4MMMMMMMMM 'L"$M$ JMMMF  "*88M
+$$P\d$$ $MMMM$L..d$r4MMMMMMMMM <$$u.e$RMMMF $M$c"$
+$ zRMM& ^8MMMMMMMMMF'MMMMMMMMM 4MMMMMMMMMG  $MMM$r
+ $RMMMF$f)MMMMMMMMMF'MMMMMMMMM 4MMMMMMMMMF.$'$MMMM
+'MMM$FJR$ $MMMMMMMMF4MMMMMMMMM 4MMMMMMMM$ $Rh^$MMM
+AMM8\dRMMF RMMMMMMM 4MMMMMMMMM  MMMMMMM$".MMMRb$MM
+AM$.$MMMMF.3MMMM$P*-'*********- "*NMMMM*..RMMMM$'$
+AP.$RMMM$:$ $M$".oM$.'$RRRRR$".d$5u'*M$ $$?RMMMM$'
+$ $MMMM$\$Rb'P eMMMMM$c"$M8# dRMMMMRc"F4MMb^$MMMMb
+ $RMMMPzRMM!  eRMMMMMMR$c" dRMMMMMMMR  'MMMR.?$MM$
+ $M8$ $MMMM"x $MMMMMMMMMM ?MMMMMMMMM$ 3'$MMM$b'$MM
+ARM$.$RMMMP $$'BMMMMMMMMM 4MMMMMMMM8P4$$ $MMMM$.$M
+A$F4RMMMMf $RM  *88MMM88M J8MM888$$\ @MMMr5MMMM$.$
+ $ $RMM8P.$MMMF?b.                z$F$MMMMc3BMMM$'
+  4MMMM$-$RMM8F4MM$ '8MMMMMMMM$ dRMM$#8MMM$r#8MMM.
+  4MMM$.$MMMM$ RMMM$ 3MMMM$ 8MMM
+   $MM$'MMMMP zRMMM$ .'**4P*".$ $MMMM$'$MMMM $MM$
+   4MMC'MMMM$:$MMMMPoM$b   .@$M$$MMMMRL^$MMMF$M8
+    "$$'MMM$\$MMMM$ MMMM$.4RMMM$r$MMMMRr*MMMN$$"
+     'N'$MM$4$MMMMF$MMMMM$$RMMMM$4$MMMM$$MMM @
+        #$MN4MMMMMF$MMMMM$#MMMMMM RMMMM$$M$F
+         ?$$.$MMMMF$MMMMMF RMMMMM $MMMM\$MP
+           *$'$MMMb3MMMMM  RMMMMNJRMMNFJ*
+ KonFucius     #88$L#8MMMr RMMM$z$M8$"
+                 ^*$P/*B8$$R8M"zP*"")
+    }
