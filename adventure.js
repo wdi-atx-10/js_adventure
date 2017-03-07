ca796c2b68349ff8394f4c27806d6641f9883c03
patientplatypus <pweyand@gmail.com>
2017-03-06 20:12:06 -0600

    done

---
+++ adventure.js
@@ -0,0 +1,26 @@
+console.log("Please put in numbers from 1 to 1000");
+var n1 = prompt("n1");
+var n2 = prompt("n2");
+var n3 = prompt("n3");
+var ntotal = n1 + n2 + n3;
+if (ntotal%7===0){
+	console.log("Welcome to destination A");
+}
+if (ntotal%7===1){
+	console.log("Welcome to destination B");
+}
+if (ntotal%7===2){
+	console.log("Welcome to destination C");
+}
+if (ntotal%7===3){
+	console.log("Welcome to destination D");
+}
+if (ntotal%7===4){
+	console.log("Welcome to destination E");
+}
+if (ntotal%7===5){
+	console.log("Welcome to destination F");
+}
+if (ntotal%7===6){
+	console.log("Welcome to destination G");
+}
