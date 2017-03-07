6685c698b3e061af647b3310f2b201c46f0765dd
Juliana Michelsen <julimichelsen@gmail.com>
2017-03-07 14:41:28 -0600

    orginal hw and then Adv2 file is redone but with prompts

---
+++ Colombia-trip.js
@@ -0,0 +1,59 @@
+
+var preferList = ['nature', 'party', 'culture'];
+var adventurous = []'1', '2', '3'];
+var visit= ['yes','no'];
+var party=['salsa','carnaval'];
+var climate=['hot', 'cold'];
+var beachMtn=['beach', 'mountains']
+var reconsider=['yes', 'no']
+
+name = prompt("what is your name?"); {
+  console.log('Hi [name]'+ "Want to visit Colombia, the happiest country in the world?")
+
+} if (visit[1]; {
+  console.log("Sorry you're a grump! Do you want to reconsider?")
+} if (reconsider[0]); {
+  console.log("Amazing! Which of the following do you prefer (pick 1): nature, party, culture?" )
+} if (reconsider[1]); {
+  console.log("You're missing out!")
+}
+  /* ADD IN ONE MORE QUESTION*/
+
+  /* YES TO VISITING CCOLOMBIA*/
+if (visit[0]); {
+  console.log("Which of the following do you prefer (pick 1): nature, party, culture?")
+  /* NATURE CITIES- Amazon, Coffee region, ciudad perdida*/
+  for (preferList[0]) {
+    console.log("Colombia is one of the most biodiverse countries in the world. On a scale of 1-3 how much do you like to rough it?");
+  } for (adventurous[0]) {
+      console.log("Discover Colombia's beautiful coffee region and the source of its famous coffee" )
+    }for (adventurous[1]) {
+      console.log("Take a tour of the Amazon rainforest!");
+    }for (adventurous[2 ]) {
+      console.log("Go on a 5 day trek and discover the Lost City")
+}
+/* PARTY CITIES- Cali, Barranquilla*/
+for (preferList[1]) {
+  console.log("Time to PARTAY! Choose between salsa dancing or carnaval" )
+}
+    for (party[0]) {
+      console.log("You're off to Cali the capital of Salsa!")
+    }
+    for (party[1]) {
+      console.log("Let the festivities begin at Barranquilla's one and only carnaval!")
+}
+/* CULTURE CITIES- BOGOTA, MEDELLIN, CARTAGENA */
+for (preferList[2) {
+  console.log("Culture abounds in Colombia. Do you prefer hot or cold climate?")
+  } for (climate[1]) {
+    console.log ("Come to Bogota! The grand capital of Colombia full of musuems and restaurants.")
+  } for (climate[0]) {
+        console.log(Do you prefer the beach or mountains?);
+      } for (beach) {
+        console.log ("Do you prefer mountains or the beach?")
+      }
+    for (beachMtn[0])
+        console.log("Welcome to the colorful coastal city of Cartagena!")
+      } for (beachMtn[1])
+        console.log("Welcome to Medellin! The most beautiful city in the mountains.")
+      }
